17b6cb328ac666b6ada2f9e825a8e6f2819c2685
dzdrum <dzdrum12@gmail.com>
2018-05-26 20:01:40 -0400

    Added automation of transfer from JSX tags to javascript for the browser

--- Indecision-App/public/scripts/app.js
+++ Indecision-App/public/scripts/app.js
@@ -1,12 +1,13 @@
-console.log('App.js is running!')
+'use strict';
+
+console.log('App.js is running!');
 
 //JSX - JavaScript XML
-//var template = <p>This is JSX from app.js!</p>; under this is a version of the same statement that the browser understands
 var template = React.createElement(
-    "p",
-    null,
-    "This is JSX from app.js!"
-  );
+  'p',
+  null,
+  'Indecision App!'
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
