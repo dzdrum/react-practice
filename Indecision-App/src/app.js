f941db9e55a6253eefaa535f6f03ec7d23292ec4
dzdrum <dzdrum12@gmail.com>
2018-06-18 15:16:09 -0400

    Created style structure and began adding styles to web app

--- Indecision-App/src/app.js
+++ Indecision-App/src/app.js
@@ -1,3 +1,7 @@
-import validator from 'validator';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
-console.log(validator.isEmail('test@gmail.com'));
+ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
